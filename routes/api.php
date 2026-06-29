1206f388301bcfb98303cdbbdd0d2b1e6a6c18cd
UssMAD <youssefmoumadd@gmail.com>
2026-06-29 11:47:07 +0100

    test postman + lab1 notes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\BlueprintController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::get('/blueprints', [BlueprintController::class, 'index']);
+Route::get('/blueprints/{blueprint}', [BlueprintController::class, 'show']);
+
